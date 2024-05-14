3c48b3e586d54d6bf9f2e0614b387e29a72d1c54
lulultfh <punyalulu05@gmail.com>
2024-05-14 14:32:21 +0700

    add member in class mahasiswa: nim, nama, setID, printAll, mahasiswa

--- static.cpp
+++ static.cpp
@@ -4,4 +4,16 @@ using namespace std;
 class mahasiswa
 {
     public:
+    static int nim;
+    string nama;
+
+    void setID();
+
+    void printAll();
+
+    mahasiswa(string pnama)
+    {
+        nama=pnama;
+        setID();
+    }
 };
