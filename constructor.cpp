11ab15b7f4a2bfb86cfc4afac6e3f5d81e690048
lulultfh <punyalulu05@gmail.com>
2024-05-14 15:32:29 +0700

    add class Mahasiswa

--- constructor.cpp
+++ constructor.cpp
@@ -1,3 +1,39 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Mahasiswa
+{
+    public:
+    int nim;
+    string nama;
+
+    public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+    Mahasiswa(int iNama)
+    {
+        nama = iNama;
+    };
+
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+    void cetak(){
+        cout << "NIM    = " << nim << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << endl;
+    };
+};
