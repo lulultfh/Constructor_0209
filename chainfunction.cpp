73cf0eed3ca1d5a78abb77a8cdcb31f18896cf27
lulultfh <punyalulu05@gmail.com>
2024-05-14 16:08:25 +0700

    add class buku

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class buku
+{
+    string judul;
+    
+    public:
+    buku setJudul(string Judul)
+    {
+        this->judul = judul;
+        return *this; //chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
